9def3be42c4348cb7a27c1e56950e621c4921017
hesmo-smm <hesham.smim@icloud.com>
2026-01-31 21:49:30 +0100

    added functionality for continues chatting

--- client.c
+++ client.c
@@ -8,7 +8,41 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>  // für inet_addr()
+#include <arpa/inet.h>
+
+// function for getting input
+void get_input(char* buffer, int size) {
+    fgets(buffer, size, stdin);
+    buffer[strcspn(buffer, "\n")] = '\0';
+}
+void close_socket(int socket_fd) {
+    if (close(socket_fd) < 0) {
+        perror("close");
+    }
+}
+void get_and_send_message(int client_fd, char* input, size_t size) {
+    // our buffer
+    if (send(client_fd, input, size, 0) < 0) {
+        perror("send");
+    }
+    printf("[SERVER]: message sent\n");
+}
+
+int read_message(int client_fd, char* buffer, size_t size) {
+    memset(buffer, 0, size);
+    ssize_t valread = read(client_fd, buffer, size -1);
+    if (valread < 0) {
+        perror("read");
+        exit(1);
+    } else if (valread == 0) {
+        printf("[CLIENT]: connection closed\n");
+        return -1;
+    } else {
+        printf("[CLIENT]: %s\n", buffer);
+        return 0;
+    }
+
+}
 
 
 int main(int argc, char *argv[]) {
@@ -18,18 +52,26 @@ int main(int argc, char *argv[]) {
     }
     char buffer[1024] = {0};
 
+    // get port from arg
     long port = strtol(argv[1], NULL, 10);
+
+    //opening socket
     printf("Opening socket to port %ld\n", port);
     int client_fd = socket(AF_INET, SOCK_STREAM, 0);
-    printf("socket created\n");
+    //validating
     if (client_fd < 0) {
         perror("socket");
         exit(1);
     }
+    printf("socket created\n");
+
+    // creating socket struct for address info
     struct sockaddr_in addr = {0};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // connecting and validating
     int status = connect(client_fd, (struct sockaddr *)&addr, sizeof(addr));
     if (status < 0) {
         perror("connect");
@@ -37,14 +79,25 @@ int main(int argc, char *argv[]) {
     }
     printf("Connected\n");
 
-    char* hello = "Hello from Client";
-    send(client_fd, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-    ssize_t valread = read(client_fd, buffer, 1024 - 1);
-    printf("read returned %ld\n", valread);
-    printf("%s\n", buffer);
-    if (close(client_fd) < 0) {
-        perror("close");
+    //get_and_send_message(client_fd);
+    //read_message(client_fd, buffer, sizeof(buffer));
+    char input[1024] = {0};
+    int pipp =0;
+
+    while (1) {
+        printf("What would you like to send?:\n");
+        get_input(input, sizeof(input));
+        if (strcmp(input, "quit") == 0 ||pipp == -1 ) {
+            break;
+        }
+        get_and_send_message(client_fd, input, strlen(input));
+        pipp = read_message(client_fd, buffer, sizeof(buffer));
+
+
+
     }
+    close_socket(client_fd);
+
+
     return 0;
 }
--- server.c
+++ server.c
@@ -5,6 +5,40 @@
 #include <unistd.h>
 #include <netinet/in.h>
 
+
+void get_input(char* buffer, int size) {
+    fgets(buffer, size, stdin);
+    buffer[strcspn(buffer, "\n")] = '\0';
+}
+void close_socket(int socket_fd) {
+    if (close(socket_fd) < 0) {
+        perror("close");
+    }
+}
+void get_and_send_message(int client_fd, char* input, size_t size) {
+    // our buffer
+    if (send(client_fd, input, size, 0) < 0) {
+        perror("send");
+    }
+    printf("[SERVER]: message sent\n");
+}
+
+int read_message(int client_fd, char* buffer, size_t size) {
+    memset(buffer, 0, size);
+    ssize_t valread = read(client_fd, buffer, size -1);
+    if (valread < 0) {
+        perror("read");
+        exit(1);
+    } else if (valread == 0) {
+        printf("[CLIENT]: connection closed\n");
+        return -1;
+    } else {
+        printf("[CLIENT]: %s\n", buffer);
+        return 0;
+    }
+
+}
+
 int main(int argc, char *argv[]) {
     // check if arguments are passed
     if (argc != 2) {
@@ -26,7 +60,7 @@ int main(int argc, char *argv[]) {
     }
     printf("socket successfully created\n");
 
-    //f*ck em up dis my port
+    // all i know is that i can use my port back2back
     if (setsockopt(server_fd, SOL_SOCKET,
                    SO_REUSEADDR | SO_REUSEPORT, &opt,
                    sizeof(opt))) {
@@ -44,7 +78,7 @@ int main(int argc, char *argv[]) {
     if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
         perror("bind failed");
         close(server_fd);
-        return 1;
+        exit(1);
     }
     printf("bind successful\n");
 
@@ -52,24 +86,21 @@ int main(int argc, char *argv[]) {
     printf("Listening for incoming connections on port %ld\n", port);
     int new_socket = accept(server_fd, (struct sockaddr*)&addr, &addr_len);
     printf("accepted fd: %d\n", new_socket);
+    char input[1024] = {0};
 
-    // reading. must remove last for \0
-    ssize_t valread = read(new_socket, buffer, 1024 - 1);
-    printf("%s\n", buffer);
-    printf("read returned %ld\n", valread);
-    char* hello = "Hello from server";
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-
-
+    while (1) {
+        int status = read_message(new_socket, buffer, sizeof(buffer));
+        printf("What would you like to send?:\n");
+        get_input(input, sizeof(input));
+        if (strcmp(input, "quit") == 0 ||status == -1 ) {
+            break;
+        }
+        get_and_send_message(new_socket, input, strlen(input));
+    }
 
 
     // validate closing socket
-    if (close(server_fd) == -1) {
-        printf("close failed\n");
-    }
-    if (close(new_socket) == -1) {
-        printf("close failed: new_socket\n");
-    }
+    close_socket(server_fd);
+    close_socket(new_socket);
     return 0;
 }
